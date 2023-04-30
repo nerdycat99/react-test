324acd9ae11a58c2b72f17d92746f8392b2e24b4
nerdycat99 <info@drysky.co.uk>
2023-04-30 23:40:52 +1000

    removed link in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,14 +8,12 @@ class Navbar extends React.Component {
         return (
             <nav className="navbar">
                 <img src={logo} className="navbar--logo" alt="logo" />
-                <h5 className="navbar--logo-text"></h5>
-                {/* <h1 className="navbar--logo-text">Australian Glass - Arts & Gifts</h1> */}
+                <h1 className="navbar--logo-text"></h1>
                 <h4 className="navbar--item">About</h4>
                 &nbsp; &nbsp;
                 <h4 className="navbar--item">Contact</h4>
                 &nbsp; &nbsp;
-                <a target="_blank" href="https://en.wikipedia.org/wiki/Cat" className="navbar--link"><img src={insta} className="navbar--insta" alt="logo" /></a>
-                {/* <img src={insta} className="navbar--insta" alt="logo" /> */}
+                <img src={insta} className="navbar--insta" alt="logo" />
             </nav>
         )
     }
