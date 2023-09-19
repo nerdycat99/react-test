00bd8f56f9d573cd90ea5dc5a485381c8e09252b
nerdycat99 <info@drysky.co.uk>
2023-09-19 12:47:41 +1000

    under construction banner

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,9 +6,10 @@ class Hero extends React.Component {
     render() {
         return (
             <section className="hero">
-                <img src={image} className="hero--image" alt="shop" />
-                <h4 className="hero--title">Level Two Queen Victoria Building Sydney</h4>
-                <p className="hero--description">Open Tuesday - Sunday, 11am - 5pm</p>
+                {/* <img src={image} className="hero--image" alt="shop" /> */}
+                <h4 className="hero--title">Under construction, coming soon...</h4>
+                {/* <h4 className="hero--title">Level Two Queen Victoria Building Sydney</h4> */}
+                {/* <p className="hero--description">Open Tuesday - Sunday, 11am - 5pm</p> */}
             </section>
         )
     }
