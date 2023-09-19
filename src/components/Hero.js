28437c7c2ebee6052e1d6262e619684895557da0
nerdycat99 <info@drysky.co.uk>
2023-09-19 12:59:56 +1000

    holding image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import image from '../images/shop.jpg';
+import image from '../images/undercons.jpg';
 
 
 class Hero extends React.Component {
@@ -7,7 +7,8 @@ class Hero extends React.Component {
         return (
             <section className="hero">
                 {/* <img src={image} className="hero--image" alt="shop" /> */}
-                <h4 className="hero--title">Under construction, coming soon...</h4>
+                <img src={image} className="hero--image" alt="shop" />
+                <h4 className="hero--title">Australian Glass, coming soon...</h4>
                 {/* <h4 className="hero--title">Level Two Queen Victoria Building Sydney</h4> */}
                 {/* <p className="hero--description">Open Tuesday - Sunday, 11am - 5pm</p> */}
             </section>
