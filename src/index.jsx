2a8b7ca0a859110a6f9575b41fa3e4762417b1cb
nerdycat99 <info@drysky.co.uk>
2023-12-01 20:48:32 +1100

    google analytics on all pages

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,24 @@
 import ReactDOM from "react-dom/client";
+import reportWebVitals from './reportWebVitals';
+import ReactGA from "react-ga4";
 import App from "./App.jsx";
 import "./index.css";
 
 const entryPoint = document.getElementById("root");
-ReactDOM.createRoot(entryPoint).render(<App />);
+//Initialize GA4
+ReactGA.initialize("G-4SBJ4GDTJW");
+ReactDOM.createRoot(entryPoint).render(
+  <>
+    <App />
+  </>
+);
+
+const SendAnalytics = ()=> {
+  // console.log("it ran.......")
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
+}
+
+reportWebVitals(SendAnalytics);
