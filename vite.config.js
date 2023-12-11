d2a957a1f592179a7576d7a2e0c796c89a4d4549
nerdycat99 <info@drysky.co.uk>
2023-12-11 18:49:28 +1100

    seo rollback use of compression

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
-import { compression } from 'vite-plugin-compression2'
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    compression(),
-    compression({ algorithm: 'brotliCompress', exclude: [/\.(br)$/, /\.(gz)$/], deleteOriginalAssets: true })
-  ]
+  plugins: [react()]
 });
